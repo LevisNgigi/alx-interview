d82d54c04b86d4fb474a8c277b0299cfd5a11486
LevisNgigi <ngigilevis@gmail.com>
2023-08-24 16:55:42 +0000

    efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""Making change O(n)"""
+"""A function to determine the fewest number of coins needed
+   to meet a given amount total"""
 
 
 def makeChange(coins, total):
-    """Clasic Bottom-Up dynamic programming"""
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
+    """
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    coins.sort()
-    for i in range(total + 1):
-        for coin in coins:
-            if coin > i:
-                break
-            if dp[i - coin] != -1:
-                dp[i] = min(dp[i - coin] + 1, dp[i])
-    return dp[total] if dp[total] != float('inf') else -1
+
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
